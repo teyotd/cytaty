bc90068e670f943dd97e09cc50ba2b55a8ce6642
Dariusz Tomaszewski <dariuszt@media-press.tv>
2023-11-27 22:14:28 +0000

    szmery bajery ide spać

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,37 +5,25 @@ import { Quotation } from './models/quotation';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // Pole do ukrywania i pokazywania formularza
-  showForm = false;
   // nowe pole reprezentującę listę cytatów
   quotes: Quotation[] = QUOTES;
-  // pole quotation reprezentuje pojedynczy cytat
-  quotation: Quotation = { author: '', sentence: '', votes: 0 };
 
   // metoda obsługuje głosowanie na konkretny cytat
   addVote(quotation: Quotation, value: number) {
     quotation.votes += value;
   }
 
-  // przełącza pole klasy true / false
-  onSwitchForm(): void {
-    this.showForm = !this.showForm;
-  }
-
-  // dodaje cytat na początek listy i resetuje pole quotation
-  addQuotation() {
-    this.quotes.unshift(this.quotation);
-    this.quotation = { author: '', sentence: '', votes: 0 };
-  }
-
   bestQuotes() {
-    return this.quotes.filter((q) => q.votes > 0);
+    return this.quotes.filter((quotation) => quotation.votes > 0);
   }
 
   worstQuotes() {
-    return this.quotes.filter((q) => q.votes < 0);
+    return this.quotes.filter((quotation) => quotation.votes < 0);
+  }
+  addNewQuotation(quotation: Quotation) {
+    this.quotes.unshift(quotation);
   }
 }
